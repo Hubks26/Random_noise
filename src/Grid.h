edcdecd978968a76cb846f66035ec8d575e83852
MAX0996 <maximehav@gmail.com>
2021-01-30 12:07:55 +0100

    deplacement curseur aleatoire

--- src/Grid.h
+++ src/Grid.h
@@ -26,6 +26,7 @@ private :
 	
 	virtual void draw(sf::RenderTarget&, sf::RenderStates) const;
 	void modif(int, int, sf::Color);
+	template<typename type> type randNb(type Min, type Max); // Méthode générique renvoyant un nb aléatoire entre Min et Max 
 	void toggleDirection();
 	
 private :
@@ -38,5 +39,14 @@ private :
 	unsigned m_level;
 	unsigned m_seed;
 };
+
+template<typename type> 
+type Grid::randNb(type Min, type Max) // Renvoie un nombre entier dans l'intervalle [Min;Max]
+{
+    type offset=0;
+    if(abs(Min) > 0) { offset = Min; }
+    type res = type(rand())/type(RAND_MAX) * (Max+1 + Min*(-1)) ;
+    return res + offset;
+}
 
 #endif // GRID_H
